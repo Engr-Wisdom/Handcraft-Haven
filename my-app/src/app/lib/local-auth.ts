b00bde98e15e689015ee62b41740b8a2bca02016
christiannataren <cnatarnlaguna@byupathway.edu>
2026-04-15 15:08:16 -0400

    seller-dasboard-session

--- my-app/src/app/lib/local-auth.ts
+++ my-app/src/app/lib/local-auth.ts
@@ -1,6 +1,18 @@
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { getUserByID } from "./data";
+
 export default async function getSessionLocal(): Promise<number> {
     const cookieStore = await cookies();
     const user_id = (cookieStore.get("user_id")?.value ?? "0") as string;
     return parseInt(user_id, 10) || 0;
+}
+
+export async function validateLogin() {
+    const id_user = await getSessionLocal();
+    if (id_user == 0) {
+        redirect("/login");
+    }
+    const user = await getUserByID(id_user);
+    return user;
 }
