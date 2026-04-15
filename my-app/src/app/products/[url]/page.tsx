5729674258b5ff94dcb65fb4cc03a3f07dc12207
Wisdom <wisdomeffion2008@gmail.com>
2026-04-15 23:27:12 +0100

    Fix build error

--- my-app/src/app/products/[url]/page.tsx
+++ my-app/src/app/products/[url]/page.tsx
@@ -1,28 +1,24 @@
-
 import { getLastReviewsByProduct, getProductByUrl, getUserByID, isProductReviewedByUser } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 import ProductView from "@/app/ui/products/product-view";
 import { Product, Review, User } from "@/app/lib/definitions";
-
 import { Metadata } from "next";
+
 type Props = {
     params: Promise<{ url: string }>
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const slug = (await params).url
-    const product = await getProductByUrl(slug)
+    const slug = (await params).url;
+    const product = await getProductByUrl(slug);
 
     return {
         title: product.name,
         description: `${product.description.slice(0, 100)} sold by ${product.store}`
-    }
+    };
 }
 
 export default async function Page(props: { params: Promise<{ url: string }> }) {
-    ///this is the route to call the products
-    //there is a column seo_url where you can found each route for the products
-    //// route example for product id 1: /products/blue-ceramic-mug
     const params = await props.params;
     const url = params.url;
     let product: Product | undefined;
@@ -31,32 +27,38 @@ export default async function Page(props: { params: Promise<{ url: string }> }) 
     let user_id = 3;
     let isReviewed: boolean;
     let name = "";
+
     if (user_id != 0) {
         [product, user] = await Promise.all([
             getProductByUrl(url),
             getUserByID(user_id),
         ]);
-        name = `${user.first_name} ${user.last_name}`;
+        name = `${user?.first_name} ${user?.last_name}`;
     } else {
         [product] = await Promise.all([
             getProductByUrl(url),
-
-
-    ]);
-
-    const name = `${user.first_name} ${user.last_name}`;
-
-
+        ]);
+        name = "";
+    }
 
     if (!product) {
         notFound();
     }
-    isReviewed = await isProductReviewedByUser(user_id, Number(product.id));
-    console.log("Reviewed: " + isReviewed)
-    const url_product = '/products/' + url;
-    reviews = await getLastReviewsByProduct(product)
-    return <>
-        <ProductView product={product} reviews={reviews} name={name} url={url_product} isReviewed={isReviewed} />
-    </>
 
+    isReviewed = await isProductReviewedByUser(user_id, Number(product.id));
+    console.log("Reviewed: " + isReviewed);
+    const url_product = '/products/' + url;
+    reviews = await getLastReviewsByProduct(product);
+
+    return (
+        <>
+            <ProductView 
+                product={product} 
+                reviews={reviews} 
+                name={name} 
+                url={url_product} 
+                isReviewed={isReviewed} 
+            />
+        </>
+    );
 }
